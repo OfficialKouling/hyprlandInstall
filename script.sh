9aa95785222a6f4484e7fd618de27b2b4cd8281e
Kouling <officialkouling@gmail.com>
2024-10-21 22:35:57 +0200

    add swhkd and enable swhkd.service

--- script.sh
+++ script.sh
@@ -8,6 +8,7 @@ sudo pacman -Sy zsh ttf-jetbrains-mono-nerd thefuck python-pywal rofi
 sudo cp -r ./rofi/themes/* /usr/share/rofi/themes/
 (sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
 (git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)
-yay -Sy paru python-pywalfox
+yay -Sy paru python-pywalfox swhkd-bin 
+systemctl --user enable --now swhkd.service
 paru wlogout
 (cd /usr/share/sddm/themes & sudo tar -xzvf ~/hyprlandInstall/sugar-candy.tar.gz & sudo sed -i "s/Current=/Current=sugar-candy/" /usr/lib/sddm/sddm.conf.d/default.conf)
