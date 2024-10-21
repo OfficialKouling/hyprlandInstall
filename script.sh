3335f8eb9cba70ab37b0f0ee12b88330206f4db2
Kouling <officialkouling@gmail.com>
2024-10-21 22:22:22 +0200

    add pywal/fox

--- script.sh
+++ script.sh
@@ -4,9 +4,9 @@ cp -r ./wallpapers ~/
 cp zshrc ~/.zshrc 
 sudo cp -r ~/ /root 
 sudo pacman -Sy hyprland hyprpaper linux-firmware waybar sddm qt5-quickcontrols2 qt5-graphicaleffects qt5-svg
-sudo pacman -Sy zsh thefuck
+sudo pacman -Sy zsh thefuck python-pywal
 (sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
 (git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)
-yay -Sy paru
+yay -Sy paru python-pywalfox
 paru wlogout
 (cd /usr/share/sddm/themes & sudo tar -xzvf ~/hyprlandInstall/sugar-candy.tar.gz & sudo sed -i "s/Current=/Current=sugar-candy/" /usr/lib/sddm/sddm.conf.d/default.conf)
