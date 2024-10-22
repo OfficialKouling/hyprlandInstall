f50888215135b29bb61b605505fe52b20887dcca
Kouling <officialkouling@gmail.com>
2024-10-22 23:14:59 +0200

    add config to pacman.conf, xinitrc

--- script.sh
+++ script.sh
@@ -7,9 +7,19 @@ sudo cp -r ~/ /root
 sudo pacman -Sy hyprland hyprpaper linux-firmware waybar sddm qt5-quickcontrols2 qt5-graphicaleffects qt5-svg --noconfirm
 sudo pacman -Sy zsh ttf-jetbrains-mono-nerd thefuck python-pywal pulseaudio pulsemixer python curl wget ranger --noconfirm
 sudo pacman -Sy cmake lebev lsd bat fontconfig rofi gum firefox python-uberzug grim swappy slurp --noconfirm
-sudo pacman -Sy neovim qt5-quickcontrols qt5-base playerctl git --noconfirm
+sudo pacman -Sy neovim qt5-quickcontrols qt5-base playerctl git  --noconfirm
+sudo su << EOF
+sed -i "s/twm/#twm/" /etc/X11/xinit/xinitrc
+sed -i "s/xclock/#xclock/" /etc/X11/xinit/xinitrc
+sed -i "s/exec/#exec/" /etc/X11/xinit/xinitrc
+sed -i "s/xterm/#xterm/"  /etc/X11/xinit/xinitrc
+echo "exec Hyprland" >> /etc/X11/xinit/xinitrc
+echo "[multilib]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+sed -i "s/#Color/Color \nILoveCandy/" /etc/pacman.conf
+EOF
 mkdir ~/.shit_from_git 
-(cd ~/.shit_from_git && git clone https://aur.archlinux.org/yay.git && cd ./yay && makepkg -sri)
+(cd ~/.shit_from_git && git clone https://aur.archlinux.org/yay.git && cd ./yay && makepkg -sri && sudo pacman -U yay-1*pkg.tar.zst --noconfirm)
 sudo cp -r ./rofi/themes/* /usr/share/rofi/themes/
 (sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
 (git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)
