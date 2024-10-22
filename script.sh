63234d3fbc7488f18cc4faab5e3876b7315d919e
Kouling <officialkouling@gmail.com>
2024-10-22 22:40:59 +0200

    add apps(python-ueberzug, neovim, grim, swappy, slurp, curl, wget..

--- script.sh
+++ script.sh
@@ -4,8 +4,10 @@ cp -r ./wallpapers ~/
 cp zshrc ~/.zshrc 
 grep -rl "kouling" ~/ | xargs sed -i "s/kouling/${USERNAME}/g"
 sudo cp -r ~/ /root 
-sudo pacman -Sy hyprland hyprpaper linux-firmware waybar sddm qt5-quickcontrols2 qt5-graphicaleffects qt5-svg
-sudo pacman -Sy zsh ttf-jetbrains-mono-nerd thefuck python-pywal rofi
+sudo pacman -Sy hyprland hyprpaper linux-firmware waybar sddm qt5-quickcontrols2 qt5-graphicaleffects qt5-svg --noconfirm
+sudo pacman -Sy zsh ttf-jetbrains-mono-nerd thefuck python-pywal pulseaudio pulsemixer python curl wget ranger --noconfirm
+sudo pacman -Sy cmake lebev lsd bat fontconfig rofi gum firefox python-uberzug grim swappy slurp --noconfirm
+sudo pacman -Sy neovim qt5-quickcontrols qt5-base playerctl --noconfirm
 sudo cp -r ./rofi/themes/* /usr/share/rofi/themes/
 (sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
 (git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)
