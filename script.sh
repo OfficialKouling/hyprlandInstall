559ae6ec04c4322905fbdacfae9dc6313a4dd2e7
Kouling <officialkouling@gmail.com>
2024-10-22 22:56:29 +0200

    fix & to &&

--- script.sh
+++ script.sh
@@ -7,11 +7,14 @@ sudo cp -r ~/ /root
 sudo pacman -Sy hyprland hyprpaper linux-firmware waybar sddm qt5-quickcontrols2 qt5-graphicaleffects qt5-svg --noconfirm
 sudo pacman -Sy zsh ttf-jetbrains-mono-nerd thefuck python-pywal pulseaudio pulsemixer python curl wget ranger --noconfirm
 sudo pacman -Sy cmake lebev lsd bat fontconfig rofi gum firefox python-uberzug grim swappy slurp --noconfirm
-sudo pacman -Sy neovim qt5-quickcontrols qt5-base playerctl --noconfirm
+sudo pacman -Sy neovim qt5-quickcontrols qt5-base playerctl git --noconfirm
+mkdir ~/.shit_from_git 
+(cd ~/.shit_from_git && git clone https://aur.archlinux.org/yay.git && cd ./yay && makepkg -sri)
 sudo cp -r ./rofi/themes/* /usr/share/rofi/themes/
 (sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)")
 (git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions)
+(git clone https://github.com/NvChad/starter ~/.config/nvim)
 yay -Sy paru python-pywalfox swhkd-bin 
 systemctl --user enable --now swhkd.service
 paru wlogout
-(cd /usr/share/sddm/themes & sudo tar -xzvf ~/hyprlandInstall/sugar-candy.tar.gz & sudo sed -i "s/Current=/Current=sugar-candy/" /usr/lib/sddm/sddm.conf.d/default.conf)
+(cd /usr/share/sddm/themes && sudo tar -xzvf ~/hyprlandInstall/sugar-candy.tar.gz && sudo sed -i "s/Current=/Current=sugar-candy/" /usr/lib/sddm/sddm.conf.d/default.conf)
